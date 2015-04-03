4a086731cdf9b10fae1e7945ef727d3f1cd874c8
Richard Grace <rgrace@nucloudglobal.com>
2015-04-03 09:24:03 -0500

    Added base url & view for site index

--- django_tut2_1/urls.py
+++ django_tut2_1/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django_tut2_1 import views
 
 urlpatterns = patterns('',
-#                        url(r'^$', include('Things.menu', namespace='menu')),
+                       url(r'^$', views.index, name='index'),
                        url(r'^Things/', include('Things.urls', namespace='Things')),
                        url(r'^user/', include('User.urls', namespace='user')),
                        
---
+++ django_tut2_1/views.py
@@ -0,0 +1,18 @@
+from django.http.response import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.shortcuts import render
+from django.template import RequestContext, loader
+
+def index(request):
+    user = request.user
+    if user is None or not user.is_active:
+        return HttpResponseRedirect(reverse('user:login'))
+    else:
+        #         URL_LIST = None
+        #         urls = get_urls()
+        urls = [{'name': 'User', 'url_name': 'user:index'},
+                {'name': 'Things', 'url_name': 'Things:index'},
+                {'name': 'Name', 'url_name': 'name:index'}
+                ]
+        context = {'urls': urls, 'user': user}
+        return render(request, 'django_tut2_1/index.html', context)
